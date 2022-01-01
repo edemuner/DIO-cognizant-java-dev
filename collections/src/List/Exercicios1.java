0a2a17cfaac553ce9ee64ca0967d47ea97982781
edemuner <eduardood15@hotmail.com>
2022-01-01 19:02:32 -0300

    programa da investigação de um crime

--- collections/src/List/Exercicios1.java
+++ collections/src/List/Exercicios1.java
@@ -1,9 +1,41 @@
 package List;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercicios1 {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        List<String> respostas = new ArrayList<>();
 
+        System.out.println("Telefonou para a vítima? [s/n]");
+        respostas.add(scan.nextLine());
+        System.out.println("Esteve no local do crime? [s/n]");
+        respostas.add(scan.nextLine());
+        System.out.println("Mora perto da vítima? [s/n]");
+        respostas.add(scan.nextLine());
+        System.out.println("Devia para a vítima? [s/n]");
+        respostas.add(scan.nextLine());
+        System.out.println("Já trabalhou com a vítima? [s/n]");
+        respostas.add(scan.nextLine());
+
+        int positivas = countPositive(respostas);
+        if (positivas == 5) System.out.println("Assassina");
+        else if (positivas >= 3) System.out.println("Cúmplice");
+        else if (positivas >= 2) System.out.println("Suspeita");
+        else System.out.println("Inocente");
+
+
+    }
+
+    public static int countPositive(List<String> list){
+        int num = 0;
+        for (String r : list){
+            if (r.equalsIgnoreCase("s")) num += 1;
+        }
+        return num;
     }
 }
