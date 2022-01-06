1347fb97a7d40d9373d0a3810c609e3c3af48df5
edemuner <eduardood15@hotmail.com>
2022-01-06 20:15:21 -0300

    teste com mock usando o framework mockito

--- testes/src/test/java/com/teste/CalculadoraTest.java
+++ testes/src/test/java/com/teste/CalculadoraTest.java
@@ -3,6 +3,8 @@ package com.teste;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -12,5 +14,16 @@ public class CalculadoraTest {
 
         int soma = calc.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testeSomaComMock(){
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+2")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
