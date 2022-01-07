b7d0b36a8af08426207871581a0a9a395ac52dca
edemuner <eduardood15@hotmail.com>
2022-01-07 17:21:12 -0300

    encapsulamento básico com getters e setters

--- desafio-banco/src/main/java/ContaCorrente.java
+++ desafio-banco/src/main/java/ContaCorrente.java
@@ -4,6 +4,18 @@ public class ContaCorrente {
     private int numero;
     private double saldo;
 
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
     public void sacar(){}
     public void depositar(){}
     public void transferir(){}
