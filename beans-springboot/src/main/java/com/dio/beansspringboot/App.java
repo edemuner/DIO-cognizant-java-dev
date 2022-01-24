5970dab8bb65b7e0a8ea4c7d4215cd162a3bdfc5
edemuner <eduardood15@hotmail.com>
2022-01-24 17:37:38 -0300

    iniciado o projeto e configurado o bean

---
+++ beans-springboot/src/main/java/com/dio/beansspringboot/App.java
@@ -0,0 +1,18 @@
+package com.dio.beansspringboot;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Livro livro = factory.getBean(Livro.class);
+        livro.setNome("Harry Potter");
+        livro.setCodigo("I4JN53J4N53");
+        livro.exibir();
+
+    }
+}
