0ea82224e79e9e5f2d19b78c1654ce078ad34f6c
edemuner <eduardood15@hotmail.com>
2022-01-30 15:24:55 -0300

    inicializado o projeto, com as dependências web e security, adicionada a dependência lombok, criada a classe UserData

---
+++ api-gateway/src/main/java/data/UserData.java
@@ -0,0 +1,18 @@
+package data;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserData implements Serializable {
+
+    private String userName;
+    private String password;
+
+
+}
