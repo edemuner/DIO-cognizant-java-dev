521170f8abf1632834bb09249af97493fed0a325
edemuner <eduardood15@hotmail.com>
2022-01-31 12:47:26 -0300

    acertados alguns pontos que geravam inconsistência

--- api-gateway/src/main/java/com/dio/apigateway/security/JWTAuthorizationFilter.java
+++ api-gateway/src/main/java/com/dio/apigateway/security/JWTAuthorizationFilter.java
@@ -2,11 +2,13 @@ package com.dio.apigateway.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,10 +17,14 @@ import java.util.ArrayList;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest req,
                                     HttpServletResponse res,
-                                    FilterChain chain) throws IOException, ServerException{
+                                    FilterChain chain) throws IOException, ServletException {
         String header = req.getHeader(SecurityConstraints.HEADER_STRING);
         if (header == null || !header.startsWith(SecurityConstraints.TOKEN_PREFIX)){
             chain.doFilter(req, res);
